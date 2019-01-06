e20d4488b100ab5496022fbf53ea972e10ef4d6b
Sergio <webdev@codwelt.com>
2019-01-06 18:46:57 -0500

    Inicio de proyecto

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,16 +5,12 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Seed the application's database.
      *
      * @return void
      */
     public function run()
     {
-//        $this->call('UsersTableSeeder');
-        $this->call(categoriapropiedades::class);
-        $this->call(metodoenvio::class);
-        $this->call(metodopago::class);
-        $this->call(tipodocumento::class);
+        // $this->call(UsersTableSeeder::class);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,15 @@
 
 /*
 |--------------------------------------------------------------------------
-| Application Routes
+| Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-$router->get('/', 'HomeController@index');
-$router->get('productos/', ['as' => 'productos', 'uses' => 'ProductosController@index']);
-$router->get('productos/agregar', ['as' => 'productosagregar', 'uses' => 'ProductosController@show']);
-$router->get('productos/propiedades', ['as' => 'productospropiedades', 'uses' => 'ProductosController@showpropiedades']);
-$router->get('productos/propiedades/categorias', ['as' => 'productospropiedadescategorias', 'uses' => 'ProductosController@showpropiedadescategorias']);
-
+Route::get('/', function () {
+    return view('welcome');
+});
