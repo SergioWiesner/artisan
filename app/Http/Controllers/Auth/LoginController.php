bceb851cdef9024cc73c710f85905c09d042840a
Sergio <webdev@codwelt.com>
2019-02-14 21:54:37 -0500

    agregando ruta de facade session

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Source\Tools\ConfiguracionInit;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
